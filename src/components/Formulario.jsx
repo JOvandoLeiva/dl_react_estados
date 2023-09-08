f50ecc92d77345af5da8111ae9026b54786213ba
Judith <judith.ovando.l@gmail.com>
2023-09-07 22:15:12 -0300

    objeto persona para que actualice estado de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,42 +3,60 @@ import socialMedia from "./SocialButton";
 
 const Login = ( {onSubmit}) => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [nombre, setNombre] = useState('')
+    const [error, setError] = useState(false)
+
+    const [personita, setPersonita] = useState({nombre:"",email:"",pass1:"",pass2:""})
  
-    const valid = (e) => {
-      //e.preventDefaul() -- esto mareaba la ejecución, se sigue recargando al validar 
-      onSubmit({email,password,nombre})
-    
+    const validar = (e) => {
+      e.preventDefault()
+      
+      if (!email.trim() || !password2.trim() || !password.trim() || !nombre.trim())
+      {
+        setError(true);
+        return;
+      }
+      setError(false);
     }
+
+    const inputHandler = (field,value) => {
+
+      setPersonita((prevPerson) => ({
+        ...prevPerson,
+        [field]:value
+      }))
+
+      setError(false)
+
+    }
+
     return (
 
-        /* aqui deberia ir una funcion que valide e indique que complete los campos */
             
         <div className="container  mx-auto">
             <header className="h1">Crea una cuenta</header>
 
-          <form onSubmit={valid}>
+          <form onSubmit={validar}>
+          
+    
               <div className="col-10  mx-auto">
             
-                <input type="text" placeholder="Nombre" className="form-control m-2" htmlFor="nombre"  id="nombre"  onChange={(e)=>setNombre(e.target.value)}/>
+                <input type="text" placeholder="Nombre" className="form-control m-2" htmlFor="nombre"  id="nombre"  onChange={(e)=> inputHandler("nombre",e.target.value)}/>
                 </div>
                 <div className="col-10  mx-auto">
                 
-                <input type="email" placeholder="tuemail@ejemplo.com" className="form-control m-2" htmlFor="email"  id="email"  onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="email" placeholder="tuemail@ejemplo.com" className="form-control m-2" htmlFor="email"  id="email"  onChange={(e)=>inputHandler("email",e.target.value)}/>
                 </div>
                <div className="col-10  mx-auto">            
-                   <input type="password" className="form-control m-2"  htmlFor="password" id="password1" placeholder="Contraseña" onChange={(p)=>setPassword(p.target.value)}/>
+                   <input type="password" className="form-control m-2"  htmlFor="password" id="password1" placeholder="Contraseña" onChange={(p)=>inputHandler("pass1",p.target.value)}/>
                   
                </div>
                     <div className="col-10  mx-auto">             
-                        <input type="password" className="form-control m-2"  htmlFor="password" id="password2" placeholder="Confirma tu contraseña" onChange={(p)=>setPassword2(p.target.value)}/>
+                        <input type="password" className="form-control m-2"  htmlFor="password" id="password2" placeholder="Confirma tu contraseña" onChange={(p)=>inputHandler("pass2",p.target.value)}/>
                     </div>
                    <div className="d-grid col-10  mx-auto">
-                     <button type="submit" className="btn btn-success" disabled={!email.trim() || !password2.trim() || !password.trim() || !nombre.trim() }>Registrarse</button>
+                     <button type="submit" className="btn btn-success">Registrarse</button>
                   </div>
+                  <div>{error ? <p>todos los campos deben estar completos</p> : null }</div>
                </form>
          </div>
             )
